ddf34dd08f38396cff59e5b873d6b0d02afcc54d
nayraarr <zita.nayra.ardini@gmail.com>
2026-02-10 00:51:25 +0700

    Create implementation of deleting product

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -6,14 +6,37 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
 
     public Product create (Product product){
+        product.setProductId(UUID.randomUUID().toString());
         productData.add(product);
         return product;
+    }
+
+    public Product findById (String idToSearch) {
+        Product result = null;
+        for (Product p: productData){
+            if (idToSearch.equals(p.getProductId())){
+                result = p;
+            }
+        }
+        return result;
+    }
+
+    public Product delete(String productId) {
+        Product existingProduct = findById(productId);
+
+        if (existingProduct != null) {
+            productData.remove(existingProduct);
+            return existingProduct;
+        }
+
+        throw new RuntimeException("Product with ID " + productId + " not found");
     }
 
     public Iterator<Product> findAll(){
