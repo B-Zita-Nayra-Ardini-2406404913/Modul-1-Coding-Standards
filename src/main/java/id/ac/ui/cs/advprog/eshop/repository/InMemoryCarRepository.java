17e7daed1d6037cfabed1a460befb4980ecff7a0
nayraarr <zita.nayra.ardini@gmail.com>
2026-03-02 23:39:08 +0700

    Implement SOLID principle to repository, make some interface to implement in car repository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/InMemoryCarRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/InMemoryCarRepository.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
+import id.ac.ui.cs.advprog.eshop.exception.CarNotFoundException;
 import id.ac.ui.cs.advprog.eshop.model.Car;
 import org.springframework.stereotype.Repository;
 
@@ -9,11 +10,12 @@ import java.util.List;
 import java.util.UUID;
 
 @Repository
-public class CarRepository {
-    static int id = 0;
+public class InMemoryCarRepository
+        implements CarReadRepository, CarWriteRepository {
 
     private List<Car> carData = new ArrayList<>();
 
+    @Override
     public Car create(Car car) {
         if (car.getCarId() == null) {
             UUID uuid = UUID.randomUUID();
@@ -23,10 +25,12 @@ public class CarRepository {
         return car;
     }
 
+    @Override
     public Iterator<Car> findAll() {
         return carData.iterator();
     }
 
+    @Override
     public Car findById(String carId) {
         for (Car car : carData) {
             if (car.getCarId().equals(carId)) {
@@ -36,6 +40,7 @@ public class CarRepository {
         return null;
     }
 
+    @Override
     public Car update(String id, Car updatedCar) {
         for (int i = 0; i < carData.size(); i++) {
             Car car = carData.get(i);
@@ -49,6 +54,7 @@ public class CarRepository {
         return null;
     }
 
+    @Override
     public void delete(String carId) {
         carData.removeIf(car -> car.getCarId().equals(carId));
     }
